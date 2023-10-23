1f886bfbd7e147e92c2e56f791cbe101eb0a9b07
Bravecode123 <teoxonjoshua13@gmail.com>
2023-10-23 09:46:55 +0800

    import savepictures

---
+++ data/pictures.js
@@ -0,0 +1,7 @@
+export const savePictures = [{
+    image: "sad"
+},
+ {
+    image: "sad"
+ },
+];
---
+++ script/main-picture.js
@@ -0,0 +1,41 @@
+import {savePictures} from "../data/pictures.js";
+
+const mainPictures =    `
+<button class = "button-left js-button-left" onclick = "addPixel();"> > </button>
+<div class = "set-of-pictures js-set-of-pictures"> 
+<div >
+<img src = "images/joshua.jpg">
+</div>
+<div>
+<img src = "images/gela.jpg">
+</div>
+<div>
+<img src = "images/YZ.jpg">
+</div>
+<div>
+<img src = "images/lesly.webp">
+</div>
+<div>
+<img src = "images/lapu.webp">
+</div>
+<div>
+<img src = "images/brody.jfif"> 
+</div>
+<div>
+<img src = "images/claude.webp"> 
+</div>
+<div>
+    <img src = "images/harl.jpg"> 
+</div>
+</div>`;
+console.log(savePictures);
+document.querySelector('.js-set-of-pictures-hide').innerHTML = mainPictures;
+
+let pixel = 0;
+function addPixel() {
+    const joshuas = document.querySelector('.js-set-of-pictures');
+        joshuas.classList.add('move-pictures');
+        pixel-=115;
+        joshuas.style.setProperty('--was', pixel + "px");
+ }
+
