5e031db8215858295439bf24894a0a129181d913
Joshua <teoxonjoshua13@gmail.com>
2024-03-31 15:31:44 +0800

    removing images

--- script/main-facebook.js
+++ script/main-facebook.js
@@ -1,5 +1,4 @@
-import {firstSideBar} from '../data/firstbardata.js';
-import {leftSideBar} from '../data/leftsidebardata.js';
-import {rightSideBar} from '../data/rightsidebardata.js';
-import {secondPicture} from '../data/secondbardata.js';
-
+import { firstSideBar } from "../data/firstbardata.js";
+import { leftSideBar } from "../data/leftsidebardata.js";
+import { rightSideBar } from "../data/rightsidebardata.js";
+import { secondPicture } from "../data/secondbardata.js";
